4077659c0f755da4bc9461ff929d5104b8ff0f9b
Joeweath <jweathingtonsr@gmail.com>
2024-02-24 01:01:47 -0600

    create variables in script for play button/gameboard and display as well as event listener for the play button and eventlistner

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World")
+const playBtn = document.getElementById('play');
+const gameBoard = document.getElementById('game-board')
+const display = document.getElementById('display')
+
+let currentPlayerTurn = "X"
+
+gameBoard.addEventListener('click', (e) => {
+    e.preventDefault()
+})
+
+playBtn.addEventListener('click', () => {
+    playBtn.setAttribute('style', 'display: none')
+    display.innerHTML = `${currentPlayerTurn} it's your turn!`
+})
+
