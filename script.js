d4a70e1a6d08ad964d7908ef0c8091a85311b148
Joeweath <jweathingtonsr@gmail.com>
2024-02-24 01:19:23 -0600

    create condition to check if tic tac toe box is already taken and displays message at bottom

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-const playBtn = document.getElementById('play');
 const gameBoard = document.getElementById('game-board')
 const display = document.getElementById('display')
 
 let currentPlayerTurn = "X"
 
+
+
 gameBoard.addEventListener('click', (e) => {
     e.preventDefault()
+    console.log(e.target.innerHTML)
+    if(e.target.innerHTML === ""){
+        e.target.innerHTML = currentPlayerTurn
+        currentPlayerTurn === "X" ? currentPlayerTurn = "O" : currentPlayerTurn = "X"
+        display.innerHTML = `${currentPlayerTurn} it's your turn!`   
+    } else {
+        display.innerHTML = "Please pick a empty box!!!"
+        return
+    }
+    
 })
 
-playBtn.addEventListener('click', () => {
-    playBtn.setAttribute('style', 'display: none')
-    display.innerHTML = `${currentPlayerTurn} it's your turn!`
-})
+
 
